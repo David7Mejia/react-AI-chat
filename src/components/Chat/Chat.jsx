4a8a9b24252ecfb5ee299b480cecf26e1563b85c
David M <david7mejia@gmail.com>
2025-03-16 15:53:33 -0400

    auto scroll

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Chat.module.css";
 import ReactMarkdown from "react-markdown";
 
+const welcomeMessage = {
+  role: "assistant",
+  content: "Hello! I'm Chatbot. How can I help you today?",
+};
+
 const Chat = ({ messages }) => {
-  const welcomeMessage = {
-    role: "assistant",
-    content: "Hello! I'm Chatbot. How can I help you today?",
-  };
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <div className={styles.Chat}>
       {[welcomeMessage, ...messages].map(({ role, content }, i) => (
         <div key={i} className={styles.Message} data-role={role}>
-          {role === "assistant" ? (
-            <ReactMarkdown>{content}</ReactMarkdown>
-          ) : (
-            content
-          )}
+          {role === "assistant" ? <ReactMarkdown>{content}</ReactMarkdown> : content}
         </div>
       ))}
+      <div ref={messageEndRef}></div>
     </div>
   );
 };
