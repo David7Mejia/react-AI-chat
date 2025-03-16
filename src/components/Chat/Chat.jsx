8e0f64188c27dd0fb1dbeb45a22d4745b4835f3f
David M <david7mejia@gmail.com>
2025-03-16 16:52:14 -0400

    fix styles auto scroll to last message group

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 import styles from "./Chat.module.css";
 import ReactMarkdown from "react-markdown";
 
-const welcomeMessage = {
-  role: "assistant",
-  content: "Hello! I'm Chatbot. How can I help you today?",
-};
+const WELCOME_MESSAGE_GROUP = [
+  {
+    role: "assistant",
+    content: "Hello! I'm Chatbot. How can I help you today?",
+    // className: "WelcomeMessage",
+  },
+];
 
 const Chat = ({ messages }) => {
   const messageEndRef = useRef(null);
+  // calc value of messageGroups using useMemo to group messages by role (assistant or user) only when messages change
+  const messageGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+
+    [messages]
+  );
 
   useEffect(() => {
     messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -16,14 +30,25 @@ const Chat = ({ messages }) => {
 
   return (
     <div className={styles.Chat}>
-      {[welcomeMessage, ...messages].map(({ role, content }, i) => (
-        <div key={i} className={styles.Message} data-role={role}>
-          {role === "assistant" ? <ReactMarkdown>{content}</ReactMarkdown> : content}
-        </div>
-      ))}
-      <div ref={messageEndRef}></div>
+      {[WELCOME_MESSAGE_GROUP, ...messageGroups].map(
+        (messages, groupIndex) => (
+          //? GROUP
+          <div key={groupIndex} className={styles.Group}>
+            {messages.map(({ role, content }, messageIndex) => (
+              //? MESSAGE
+              <div key={messageIndex} className={styles.Message} data-role={role}>
+                <ReactMarkdown>{content}</ReactMarkdown>
+                {/* {role === "assistant" ? <ReactMarkdown>{content}</ReactMarkdown> : content} */}
+              </div>
+            ))}
+          </div>
+        )
+
+        // {[WELCOME_MESSAGE_GROUP, ...messages].map(({ role, content }, i) => (
+        // ))}
+      )}
+      <div className="end-ref" ref={messageEndRef}></div>
     </div>
   );
 };
-
 export default Chat;
